8583e73f672790697e1a8e7ffdadb8465724592e
Pia Pringle <ppringle@vmware.com>
2023-02-10 16:37:50 -0500

    first commit

---
+++ src/main/java/com/pringweb/mongodb/SpringBootMongoIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.pringweb.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongoIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongoIntegrationApplication.class, args);
+	}
+
+}
